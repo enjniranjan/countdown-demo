24c4cfd6fe8e2311e5ff489fe6c9727f8ddda37f
root <>
2021-06-29 22:19:45 +0530

    Added basic countdown design

--- src/app/components/count-down/count-down.component.ts
+++ src/app/components/count-down/count-down.component.ts
@@ -1,52 +1,69 @@
-import { Component, OnInit,OnDestroy } from '@angular/core';
-import { Subscription, interval } from 'rxjs';
+import { Component, VERSION, Input } from '@angular/core';
+import { formatDate } from '@angular/common';
 
+import { BehaviorSubject, timer } from 'rxjs';
+import { switchMap, map, takeWhile, pairwise } from 'rxjs/operators';
+
+import {
+  trigger,
+  transition,
+  animate,
+  keyframes,
+  style
+} from '@angular/animations';
 @Component({
   selector: 'count-down',
   templateUrl: './count-down.component.html',
-  styleUrls: ['./count-down.component.scss']
+  styleUrls: ['./count-down.component.scss'],
+  animations: [
+    trigger('flip', [
+      transition('*=>*', [
+        animate(
+          '.6s',
+          keyframes([
+            style({ transform: 'rotateX(130deg)', offset: 0 }),
+            style({ transform: 'rotateX(0deg)', offset: 0.45 }),
+            style({ transform: 'rotateX(7deg)', offset: 0.5 }),
+            style({ transform: 'rotateX(0deg)', offset: 0.53 }),
+            style({ transform: 'rotateX(5deg)', offset: 0.56 }),
+            style({ transform: 'rotateX(0deg)', offset: 0.6 }),
+            style({ transform: 'rotateX(0deg)', offset: 0.95 }),
+            style({ transform: 'rotateX(0deg)', offset: 1 })
+          ])
+        )
+      ])
+    ])
+  ]
 })
-export class CountDownComponent implements OnInit, OnDestroy {
+export class CountDownComponent {
+  change: boolean = false;
+  showShadow = [];
+  name = 'Angular ' + VERSION.major;
+  initialMinutes$ = new BehaviorSubject(30);
 
-  private subscription: any;
-  
-    public dateNow = new Date();
-    public dDay = new Date('July 10 2021 00:00:00');
-    milliSecondsInASecond = 1000;
-    hoursInADay = 24;
-    minutesInAnHour = 60;
-    SecondsInAMinute  = 60;
-
-    public timeDifference:any;
-    public secondsToDday:any;
-    public minutesToDday:any;
-    public hoursToDday:any;
-    public daysToDday:any;
-
-    constructor() { 
-   // Initialization inside the constructor
-    }
-
-
-    private getTimeDifference () {
-        this.timeDifference = this.dDay.getTime() - new  Date().getTime();
-        this.allocateTimeUnits(this.timeDifference);
-    }
-
-  private allocateTimeUnits (timeDifference:any) {
-        this.secondsToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond) % this.SecondsInAMinute);
-        this.minutesToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour) % this.SecondsInAMinute);
-        this.hoursToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute) % this.hoursInADay);
-        this.daysToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute * this.hoursInADay));
+  @Input()
+  set minutes(val:any) {
+    this.initialMinutes$.next(val);
   }
-
-    ngOnInit() {
-       this.subscription = interval(1000)
-           .subscribe(x => { this.getTimeDifference(); });
-    }
-
-   ngOnDestroy() {
-      this.subscription.unsubscribe();
-   }
-
+  value = 0;
+  timer$ = this.initialMinutes$.pipe(
+    map(minutes => minutes * 60000 + new Date().getTime()),
+    switchMap(minutes =>
+      timer(0, 1000).pipe(
+        takeWhile(() => minutes - new Date().getTime() > 0),
+        map(t =>
+          formatDate(
+            minutes - new Date().getTime(),
+            'ddHHmmss',
+            'en-US',
+            '+0000'
+          ).split('')
+        ),
+        pairwise(),
+        map(([old, value]) => {
+          return value.map((x, index) => ({ value: x, old: old[index] }));
+        })
+      )
+    )
+  );
 }
